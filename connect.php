1d64ef19ff1f34baa312b6f3aa18f7a89c27c10e
qdlyh <liang620523@163.com>
2017-04-25 10:49:54 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 //$con = new mysqli("localhost","root","377586050",'runoob');
- $con=@mysql_connect('localhost','root',"377586050");
+ $con=@mysql_connect('localhost','root',"");
 // 检测连接
 if ($con) {
     echo "连接成功"."<br>";
